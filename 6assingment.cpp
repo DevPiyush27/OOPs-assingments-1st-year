76756cf1a7810fa144ff40ce1a062c0e36b8d958
piyush <piyushpatil27102008@gmail.com>
2026-09-17 12:23:42 +0530

    Add files via upload

---
+++ 6assingment.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student {
+public:
+    int rno;
+    string name;
+    float marks; // Added third variable
+
+    
+    student(int rno, string name, float marks) {
+        this->rno = rno;
+        this->name = name;
+        this->marks = marks;
+        cout << "Student record created!!!!" << endl;
+    }
+
+
+    ~student() {
+        cout << "Student record deleted!!!!" << endl;
+    }
+
+    void display() {
+        cout << "Roll NO : " << rno << endl;
+        cout << "Name : " << name << endl;
+        cout << "Marks : " << marks << endl;
+    }
+};
+
+int main() {
+    
+    student s1(101, "Rahul", 88.5);
+
+    
+    s1.display();
+
+    return 0;
+}
