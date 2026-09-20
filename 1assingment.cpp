b1e1faedb33140af94228727eb8ec0b63c02b2c6
piyush <piyushpatil27102008@gmail.com>
2026-09-20 10:23:25 +0530

    Update 1assingment.cpp

--- 1assingment.cpp
+++ 1assingment.cpp
@@ -1,52 +1,50 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class books {
 public:
     float price;
     int total_pages;
     int year;
-    std::string name;
+    string name;
 
     void pricedisplay() {
-        std::cout << "the price of book is = " << price << std::endl;
+        cout << "the price of book is = " << price << endl;
     }
 
     void total_page_display() {
-        std::cout << "the total pages of book are = " << total_pages << std::endl;
+        cout << "the total pages of book are = " << total_pages << endl;
     }
 
     void year_release() {
-        std::cout << "the year of publication is = " << year << std::endl;
+        cout << "the year of publication is = " << year << endl;
     }
 
     void name_book() {
-        std::cout << "Name of the book is = " << name << std::endl;
+        cout << "Name of the book is = " << name << endl;
     }
 };
 
 int main() {
     books b1;
 
-    std::cout << "enter the number of total pages: " << std::endl;
-    std::cin >> b1.total_pages;
+    cout << "enter the number of total pages: " << endl;
+    cin >> b1.total_pages;
 
-    std::cout << "enter the name of the book: " << std::endl;
-    std::cin.ignore(); 
+    cout << "enter the name of the book: " << endl;
+    cin.ignore();
+    getline(cin, b1.name);
 
-    std::getline(std::cin, b1.name);
+    cout << "enter the price of the book: " << endl;
+    cin >> b1.price;
 
-    std::cout << "enter the price of the book: " << std::endl;
-    std::cin >> b1.price;
+    cout << "enter the year of publication: " << endl;
+    cin >> b1.year;
 
-    std::cout << "enter the year of publication: " << std::endl;
-    std::cin >> b1.year;
-
-
-    std::cout<< "========Book Details========" << std::endl;
+    cout << "========Book Details========" << endl;
 
     b1.total_page_display();
-
     b1.name_book();
     b1.pricedisplay();
     b1.year_release();
